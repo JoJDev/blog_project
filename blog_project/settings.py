c44b755dbe91324a8a3016153be8c062f1c91393
Daniel Ortiz <thisisdanielortiz@gmail.com>
2022-08-15 17:30:22 -0500

    Categories (model and migrations)

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog_centre.apps.BlogCentreConfig',
     'user',
+    'categories',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
