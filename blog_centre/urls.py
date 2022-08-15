7ab979343891f017a4db9ba33fcada874d58fe2a
Daniel Ortiz <thisisdanielortiz@gmail.com>
2022-08-15 18:05:27 -0500

    Categories serializers and endpoints

--- blog_centre/urls.py
+++ blog_centre/urls.py
@@ -6,6 +6,7 @@ from user.views import RegisterUser, UserView
 urlpatterns = [
     path('blog/', include(post_router.urls)),
     path('auth/', include('user.api.urls')),
+    path('categories', include('categories.urls')),
     path('auth/register', RegisterUser.as_view(), name="register_user"),
     path('auth/me', UserView.as_view(), name="me")
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Category
+from .serializers import CategorySerializer
+
+from django.shortcuts import get_object_or_404
+# from rest_framework.permissions import IsAuthenticated
+
+
 
 # Create your views here.
+class CategoryView(APIView):
+    
+    def get(self, request):
+        all_cat = Category.objects.all()
+        serializer = CategorySerializer(all_cat, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
