c912cd79e4b64ad98079439434512c8ffe23adb5
Daniel Ortiz <thisisdanielortiz@gmail.com>
2022-08-16 11:34:30 -0500

    categories filter and url

--- categories/urls.py
+++ categories/urls.py
@@ -3,5 +3,5 @@ from .views import CategoryView, CategoryDetailView
 
 urlpatterns = [
     path('', CategoryView.as_view(), name='category_list'),
-    path('<int:pk>', CategoryDetailView.as_view(), name='category_detail')
+    path('<slug>', CategoryDetailView.as_view(), name='category_detail')
 ]
