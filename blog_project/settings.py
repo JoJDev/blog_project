fa61297d83a1510200344e97bc4471540aa4dc39
Daniel Ortiz <thisisdanielortiz@gmail.com>
2022-08-17 12:37:25 -0500

    delpoy prep

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gq3g7jgsok%p*=)gtvpxk#3*5vg9z@h(m#dei*4ay&9^hjea46'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
