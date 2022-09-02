3f8b3e7a216a1e37df42d4c4de70f7c091cf1235
JoJDev <jojdeveloper@gmail.com>
2022-09-02 10:56:52 -0500

    README added

---
+++ README.md
@@ -0,0 +1,32 @@
+# TechBlog project
+*Not final name*
+
+A small blog website to practice with technologies  which as Django for backend and React for frontend
+
+
+## Instalation
+
+Install Django server (from root directory)
+~~~
+cd backend_django 
+python -m venv venv
+
+.\venv\Scripts\activate  # Windows
+    or
+source bin/activate      # Linux
+
+pip install -r requirements.txt
+python manage.py run server 
+~~~
+
+React commands
+~~~
+npm install
+npm run dev
+~~~
+
+---
+Backend: [Daniel-Ortiz](https://github.com/Daniel-Ortiz1210/)
+Frontend: [JoJDev](https://github.com/JoJDev/)
+
+
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import './Footer.scss'
 function Footer() {
     return (
         <footer>
-                <p className='footer__dev'>Backend e idea de la página creada por <a href="">Daniel Ortiz</a></p>
+                <p className='footer__dev'>Backend e idea de la página creada por <a href="https://github.com/Daniel-Ortiz1210">Daniel Ortiz</a></p>
                 <h5 className='footer__licencia'>TechBlog </h5>
-                <p className='footer__dev'>Frontend de la página creado por <a href="">JoJDev</a></p>
+                <p className='footer__dev'>Frontend de la página creado por <a href="https://github.com/JoJDev/">JoJDev</a></p>
         </footer>
     );
 }
