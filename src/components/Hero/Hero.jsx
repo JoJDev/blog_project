f763a35fafa2f25c90e892098b1743006cf2fc75
JoJDev <jojdeveloper@gmail.com>
2022-09-07 12:04:35 -0500

    Hero header created and nav modificated

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import './Hero.scss';
+
+function Hero() {
+    return (
+        <section className='hero_section'>
+            <h1>TechBlog</h1>
+            <h2>En el mundo de la tecnología para tí</h2> 
+        </section>
+    )
+}
+
+export default Hero
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,18 @@
 import Header from './../components/Header/Header';
 import Main from './../components/Main/Main';
 import Footer from './../components/Footer/Footer';
+import Hero from '../components/Hero/Hero';
 
 export default function HomePage() {
     return (
         <>
             <Header />
-            {/* <Hero /> */}
+            <Hero />
+            <h3>Ultimos posts</h3>
+            <h3>Posts segun categorias</h3>
+            <h3>Ultimos comentarios</h3>
+
+
             <Main />
             <Footer />
         </>
